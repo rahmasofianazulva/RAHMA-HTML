296f91ddf7743d5d395af485f371f277ffba2a48
rahmasofianazulva <165308569+rahmasofianazulva@users.noreply.github.com>
2024-03-29 00:15:54 +0700

    Add files via upload

---
+++ HASIL.php
@@ -0,0 +1,143 @@
+!DOCTYPE html>
+<html>
+<head>
+       <title>Biodata Diri Rahma Sofiana Zulva</title>
+</head>
+<body>
+      <h1 style="text-align:center;">NAMA PROFIL</h1>
+	  <table border="1" cellspacing="0" align="center" cellpadding="5" width="800">
+	  <tr align="center">
+	  <th>FOTO</th>
+		  <td><img src="C:\Users\khari\OneDrive\Documents\rahmasofianazulva.jpg" alt=rahmasofianazulva.jpg width=300 height=400 </td>
+	  </tr>
+	  <tr>
+	      <td>NAMA</td>
+		  <td>RAHMA SOFIANA ZULVA</td>
+	  </tr>
+	  <tr>
+	      <td>TTL</td>
+		  <td>Batang, 23 November 2005</td>
+	  </tr>
+	  <tr>
+	      <td>AGAMA</td>
+		  <td><div>
+			<input type="checkbox" id="Islam" name="Islam"
+			<label for="Islam">Islam</label>
+	  </div>
+		  <div>
+		  <input type="checkbox" id="Kristen" name="Kristen"
+		  <label for="Kristen">Kristen</label>
+		  <div>
+			<input type="checkbox" id="Katolik" name="Katolik"
+			<label for="Katolik">Katolik</label>
+	  </div>
+	  		  <div>
+			<input type="checkbox" id="Hindu" name="Hindu"
+			<label for="Hindu">Hindu</label>
+	  </div>
+	  		  <div>
+			<input type="checkbox" id="Budha" name="Budha"
+			<label for="Budha">Budha</label>
+	  </div>
+	  		  <div>
+			<input type="checkbox" id="Khonghucu" name="Khonghucu"
+			<label for="Khonghucu">Khonghucu</label>
+	  </div>
+	  </td>
+	  </div>
+	  </tr>
+	  <tr>
+	      <td>JENIS KELAMIIN</td>
+		  <td><select name="jeniskelamin">
+		  <option>Perempuan</option>
+		  <option>Laki-laki</option>
+		  </select>
+	  </tr>
+	  <tr>
+	     <td>PEKERJAAN</td>
+		  <td><select name="pekerjaan">
+		  <option>Mahasiswa</option>
+		  <option>PNS</option>
+		  <option>BUMN</option>
+		   <option>Wiraswasta</option>
+		    <option>Dosen</option>
+		  </select>
+	  </tr>
+	  <tr>
+	  <td>HOBI</td>
+		  <td><ol>
+       <li>Mendengarkan Musik</li>
+            <ul>
+                <li>Arabic</li>
+                <li>English</li>
+                <li>DJ Remix</li>
+            </ul>
+       </li>
+        <li>Olahraga
+            <ul>
+                <li>Renang</li>
+				<li>Lari</li>
+				<li>Volly</li>
+            </ul>
+        </li>
+        <li>Makan</li>
+		<ul>
+                <li>Seblak</li>
+                <li>Bakso</li>
+                <li>Kue Pancong</li>
+            </ul>
+	</td>
+    </ol>
+	</tr>
+	  <tr>
+	  <td>MOTIVASI</td>
+	      <td><ul>
+	       <li>Selesaikan apa yang sudah kita mulai!</li>
+		   <li>Fokus pada tujuan, bukan hambatan!</li>
+		   <li>Jika kamu lelah, ingatlah orang tuamu lebih lelah!</li>
+	  </td>
+	  </tr>
+	  <tr>
+	      <td>NIM</td>
+	      <td>23030360022</td>
+	  </tr>
+	  <tr>
+	    <td>KELAS</td>
+		<td>2A</td>
+	  </tr>
+	 <tr>
+<td>Jurusan</td>
+<td> <input type="radio" id="PAI" name="Jurusan" value="PAI">
+  <label for="PAI">PAI</label><br>
+  <input type="radio" id="MPI" name="Jurusan" value="MPI">
+  <label for="MPI">MPI</label><br>
+  <input type="radio" id="PBA" name="Jurusan" value="PBA">
+  <label for="PBA">PBA</label><br>
+  <input type="radio" id="PBI" name="Jurusan" value="PBI">
+  <label for="PBI">PBI</label><br>
+  <input type="radio" id="PIAUD" name="Jurusan" value="PIAUD">
+  <label for="PIAUD">PIAUD</label><br>
+  <input type="radio" id="PGMI" name="Jurusan" value="PGMI">
+  <label for="PGMI">PGMI</label></td>
+</tr>
+	  <tr>
+	  <td>KULIAH</td>
+	  <td>UIN WALISONGO SEMARANG <a href="https://walisongo.ac.id/">klik disini</a></td>
+	  </tr>	  
+	  <tr>
+	  <td>PHONE</td>
+	  <td>085712082053 </td>
+	  </tr>
+	  <tr>
+	  <td>EMAIL</td>
+	  <td><a href="sofianarahma410@gmail.com">sofianarahma410@gmail.com</a></td>
+	  </tr>
+	  <tr>
+	  <td>DESKRIPSI DIRI</td>
+	  <td>Rahmaa anak yang comell, kiyowo, baik, pintar, tidak sombong, rajin menabung, dan suka menolong</td>
+	  </tr>
+	  </table>
+<body>
+ <p>link html rahmaa disini ya pak saiful <a href="">link html</a></p>
+</body>
+</html>
